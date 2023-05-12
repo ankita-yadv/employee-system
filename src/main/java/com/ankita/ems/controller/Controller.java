475a2cea9b5302a13e81c76373f309a9097a529e
Ankita Yadav <ankita2016yadav@gmail.com>
2023-05-12 22:23:19 +0530

    formatted and added comments

--- src/main/java/com/ankita/ems/controller/Controller.java
+++ src/main/java/com/ankita/ems/controller/Controller.java
@@ -12,39 +12,70 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ankita.ems.exception.RecordNotFoundException;
 import com.ankita.ems.model.Employee;
 import com.ankita.ems.service.EmployeeService;
 
+
+/**
+ * Rest controller for the API
+ */
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
 	@Autowired
 	private EmployeeService employeeService;
-	
+
+	/**
+	 * Fetches all employees data
+	 * 
+	 * @return List of employees
+	 */
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
 	}
-	
+
+	/**
+	 * Fetch data of an employee
+	 * 
+	 * @return Employee, if present against the id
+	 * @throws RecordNotFoundException
+	 */
 	@GetMapping("/employees/{id}")
-	public Employee getEmployee(@PathVariable Integer id){
+	public Employee getEmployee(@PathVariable Integer id) throws RecordNotFoundException {
 		return employeeService.getEmployeeByID(id);
 	}
-	
+
+	/**
+	 * Save the employee data received from the request
+	 * 
+	 * @return Employee data saved in the DB with the generated id
+	 */
 	@PostMapping("/employees")
-	public Employee saveEmployee(@RequestBody Employee employee){
-		System.out.println(employee);
+	public Employee saveEmployee(@RequestBody Employee employee) {
 		return employeeService.createEmployee(employee);
 	}
-	
+
+	/**
+	 * Update the employee data against the id given
+	 * 
+	 * @return List of employees
+	 * @throws RecordNotFoundException
+	 */
 	@PutMapping("/employees/{id}")
-	public Employee updateEmployee(@PathVariable Integer id){
+	public Employee updateEmployee(@PathVariable Integer id) throws RecordNotFoundException {
 		return employeeService.updateEmployee(id);
 	}
-	
+
+	/**
+	 * Delete employee data against the id provided if present
+	 * 
+	 * @throws RecordNotFoundException
+	 */
 	@DeleteMapping("/employees/{id}")
-	public void deleteEmployeeByID(@PathVariable Integer id){
+	public void deleteEmployeeByID(@PathVariable Integer id) throws RecordNotFoundException {
 		employeeService.deleteEmployee(id);
 	}
 }
